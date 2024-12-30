c7c10fe536ed2d402dd095b97f69027ba7a296b7
Adam Fehse <adamfehse@arizona.edu>
2024-12-30 14:35:11 -0700

    Updates

---
+++ _next/static/7vauho28ONxYjpG8t2eGp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/986-5ec52221c11ff14c.js","static/css/a0491b9577c4a6a4.css","static/chunks/pages/index-32b96f9608880075.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
